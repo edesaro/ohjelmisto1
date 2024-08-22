d670b110fb27b6e8c446141729f99ad7f0285bcf
nice.pavliuk <nice.pavliuk@gmail.com>
2024-08-22 15:25:24 +0300

    moi

---
+++ moduuli 2/mod2.py
@@ -0,0 +1,24 @@
+# Syötteen lukeminen
+name = input ("Anna nimesi: ")
+# name = "Matti"
+# \ on escape-merkki, jolla voi tuottaa esim. tabin tai rivi
+print("Moi ´\t " + name) # "Moi " + Matti" -> "Moi Matti"
+
+# age = "7"
+# käyttäjän syöte on aina merkkijono
+
+print("Anna ikäsi; " + age)
+# Muutetaan merkkijono (str) kokonaisluvuksi (int) ja lisätään
+
+age = int(age) + 1 # "7" -> 7 + 1 -> = 8
+age_string = str(age) # muutetaan  int -> string, esim. 8 -> "8"
+print("Ikäsi on vuoden päästä" + age)
+
+age = age + 1
+print("Ikäsi on kahden vuoden päästä " + str(age))
+
+# käyttäjän pituus metreinä liukuluku (float)
+height = input("Anna pituus: ")
+
+# tulos f-string muodossa, ei tarvita  str() - funktiota
+print(f"Nimi: {name}, Ikä: {age*2}, Pituus: {height}." )
---
+++ moduuli 2/teht2.py
@@ -0,0 +1,14 @@
+# Kirjoita ohjelma, jokaa kysyy ympyrän säteen ja tulostaa sen pinta-alan.
+# piir*r2
+import math
+
+r = float(input("Anna ympyrän säde (m): "))
+#ympyran_sade circle_radius
+area = math.pi * r * r
+print(f"Ympyrän, jona säde on {r}, pinta-ala on {area:.1f} neliömetriä.")
+
+
+
+
+
+
